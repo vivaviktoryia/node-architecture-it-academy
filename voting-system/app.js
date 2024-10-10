19ba3eccb46ea248164474b5f67390ecea801d7a
Viktoryia <viktoryiaantonchyk@gmail.com>
2024-10-10 12:41:39 +0300

    changes xss middleware which sanitizes user input data

--- voting-system/app.js
+++ voting-system/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const xss = require('xss-clean'); // Data sanitization - XSS
+const { xss } = require('express-xss-sanitizer'); // Data sanitization - XSS
 
 const {
 	logLineSync,
