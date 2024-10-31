508c1acc94826b254f4a82771c23d0fdb0aa0b8a
Viktoryia Antonchyk <viktoryiaantonchyk@gmail.com>
2024-10-31 17:52:41 +0300

    restructure script.js

---
+++ my-postman/public/js/popup.js
@@ -0,0 +1,11 @@
+const hidePopup = () => {
+	const el = document.querySelector('.popup');
+	if (el) el.parentElement.removeChild(el);
+};
+
+export const displayPopup = (type, msg) => {
+	hidePopup();
+	const markup = `<div class="popup popup--${type}">${msg}</div>`;
+	document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+	window.setTimeout(hidePopup, 5000);
+};
