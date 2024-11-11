a234eee8ef027bbfd93c2ba8e34ef290b5b87862
Viktoryia <viktoryiaantonchyk@gmail.com>
2024-11-11 21:31:26 +0300

    remove duplicates of validation forms folders

--- validation-form-handlebars/routes/errorRotes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const { handleNotFound } = require('../controllers/errorController');
-
-const router = express.Router();
-
-router.route('*').all(handleNotFound);
-
-module.exports = router;
