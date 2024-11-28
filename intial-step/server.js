ee3c909274265da263e2270a145bae34c238c8aa
Viktoryia Antonchyk <viktoryiaantonchyk@gmail.com>
2024-11-28 17:53:14 +0300

    added initial setup for project

---
+++ intial-step/server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: `${__dirname}/.env` });
+
+const app = require('./app');
+const port = process.env.PORT || 7181;
+
+const { logLineSync, logLineAsync } = require('./src/utils/logger');
+
+const logFileName = '_server.log';
+const logFilePath = path.resolve('logs', logFileName);
+
+process.on('uncaughtException', (err) => {
+	const logLine = `UNCAUGHT EXCEPTION!💥 Shutting down... ${err.name}: ${err.message}`;
+	logLineSync(logFilePath, logLine);
+	process.exit(1);
+});
+
+const server = app.listen(port, async () => {
+	const logLine = `Web server running on port  ${port}, process.pid = ${process.pid}`;
+	await logLineAsync(logFilePath, logLine);
+});
+
+process.on('unhandledRejection', (err) => {
+	const logLine = `UNHANDLED REJECTION!💥 Shutting down... ${err.name}: ${err.message}`;
+	logLineSync(logFilePath, logLine);
+	server.close(() => {
+		process.exit(1);
+	});
+});
