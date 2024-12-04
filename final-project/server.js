1c46a037651789d92252c4dc62d0a701f497f263
Viktoryia Antonchyk <viktoryiaantonchyk@gmail.com>
2024-12-04 12:14:56 +0300

    initial setup

---
+++ final-project/server.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+const { logError, logInfo } = require('./utils/logger');
+
+dotenv.config({ path: `${__dirname}/.env` });
+
+const app = require('./app');
+const port = process.env.PORT || 7181;
+
+process.on('uncaughtException', async (err) => {
+	const logLine = `UNCAUGHT EXCEPTION! 💥 ${err.name}: ${err.message}`;
+	await logError(logLine);
+	process.exit(1);
+});
+
+const server = app.listen(port, async () => {
+	const logLine = `Web server running on port ${port}, process.pid = ${process.pid}`;
+	await logInfo(logLine);
+});
+
+process.on('unhandledRejection', async (err) => {
+	const logLine = `UNHANDLED REJECTION!💥 Shutting down... ${err.name}: ${err.message}`;
+	await logError(logLine);
+	server.close(() => {
+		process.exit(1);
+	});
+});
